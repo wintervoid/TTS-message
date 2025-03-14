72001fa3825b22d36a75e73bd03d22abf94b7b7a
Caleb67 <57026376+Caleb67@users.noreply.github.com>
2025-03-13 17:40:54 -0700

    I GOTTA FIX THE ASM, I DID IT WRONG
    forgot how pointers work omegalul

--- tts.asm
+++ tts.asm
@@ -1,14 +1,15 @@
 org 0x7c00
 
 boot:
-    mov al, message ; our message
+    mov bh, message ; our message
     mov ah, 0x0e ; bios command to print
 .loop:
-    ; al holds pointer to the message
+    ; bh now holds pointer to the message
+    mov al, [bh] ; we have to dereference this first (i forgot, omegalul)
     cmp al, 0 ; if it is null we have reached the end
     je halt
     int 0x10 
-    add al, 1 ; next byte
+    add bh, 1 ; next byte
     jmp .loop
 
 halt:
