4bcc45343a0d30f91fcc13d577843d74730f766d
Caleb67 <57026376+Caleb67@users.noreply.github.com>
2025-03-13 18:14:09 -0700

    Create tts.rs
    omegalul

---
+++ tts.rs
@@ -0,0 +1,8 @@
+pub fn give_me_the_tts() -> String {
+    return String::from("Hey Doug, I'm worried that our friendship is only parasocial, can you acknowledge this message so that it isn't anymore?");
+}
+
+
+fn main() {
+    println!("{}", give_me_the_tts());
+}
